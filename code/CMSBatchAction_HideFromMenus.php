7609881b65249e6910fa3a3e9a437149430e9cc4
helpfulrobot <chris@silverstripe.com>
2015-12-17 20:48:18 +1300

    Converted to PSR-2

--- code/CMSBatchAction_HideFromMenus.php
+++ code/CMSBatchAction_HideFromMenus.php
@@ -6,34 +6,37 @@
 * @package BatchActionsPlus
 */
 
-class CMSBatchAction_HideFromMenus extends CMSBatchAction {
-	public function getActionTitle() {
-		return _t('CMSBatchActions.HIDEFROMMENUS', 'Hide from menus');
-	}
+class CMSBatchAction_HideFromMenus extends CMSBatchAction
+{
+    public function getActionTitle()
+    {
+        return _t('CMSBatchActions.HIDEFROMMENUS', 'Hide from menus');
+    }
 
-	public function run(SS_List $pages) {
+    public function run(SS_List $pages)
+    {
+        $status = array(
+            'modified'=>array()
+        );
 
-		$status = array(
-			'modified'=>array()
-		);
+        foreach ($pages as $page) {
+            $id = $page->ID;
 
-		foreach($pages as $page) {
-			$id = $page->ID;
+            // Perform the action
+            $page->ShowInMenus = 0;
+            $page->write();
 
-			// Perform the action
-			$page->ShowInMenus = 0;
-			$page->write();
+            $status['modified'][$id] = array(
+                'TreeTitle' => $page->TreeTitle
+            );
+        }
 
-			$status['modified'][$id] = array(
-				'TreeTitle' => $page->TreeTitle
-			);
-		}
-
-		return $this->response(_t('CMSBatchActions.HIDDENMENUS', 'Hidden from menus'), $status);
-	}
+        return $this->response(_t('CMSBatchActions.HIDDENMENUS', 'Hidden from menus'), $status);
+    }
 
 
-	public function applicablePages($ids) {
-		return $this->applicablePagesHelper($ids, 'canEdit', false, true);
-	}
+    public function applicablePages($ids)
+    {
+        return $this->applicablePagesHelper($ids, 'canEdit', false, true);
+    }
 }
